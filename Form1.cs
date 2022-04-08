d29caf0d5d1c678c4ca3ab4e1053e5ee944b60fe
JESUS GOMEZ <jesussgomez@ucundinamarca.edu.co>
2022-04-07 20:35:54 -0500

    crud

--- Form1.cs
+++ Form1.cs
@@ -41,6 +41,7 @@ namespace tienda
                 MySqlCommand comando = new MySqlCommand(sql, conexionBD);
                 comando.ExecuteNonQuery();
                 MessageBox.Show("Registro guardado");
+                limpiar();
             }
             catch(MySqlException ex)
             {
@@ -74,14 +75,14 @@ namespace tienda
                     while (reader.Read())
                     {
                         txtCodigo.Text = reader.GetString(0);
-                        txtMarca.Text = reader.GetString(1);
-                        txtTipoProducto.Text = reader.GetString(2);
-                        txtCategoria.Text = reader.GetString(3);
-                        txtColor.Text = reader.GetString(4);
-                        txtEnvase.Text = reader.GetString(5);
-                        txtTamaño.Text = reader.GetString(6);
-                        txtPrecio.Text = reader.GetString(7);
-                        txtCantidad.Text = reader.GetString(8);
+                        txtMarca.Text = reader.GetString(2);
+                        txtTipoProducto.Text = reader.GetString(3);
+                        txtCategoria.Text = reader.GetString(4);
+                        txtColor.Text = reader.GetString(5);
+                        txtEnvase.Text = reader.GetString(6);
+                        txtTamaño.Text = reader.GetString(7);
+                        txtPrecio.Text = reader.GetString(8);
+                        txtCantidad.Text = reader.GetString(9);
                     }
                 }else
                 {
@@ -115,7 +116,7 @@ namespace tienda
             double precio = double.Parse(txtPrecio.Text);
             int cantidad = int.Parse(txtCantidad.Text);
 
-            string sql = "UPDATE productos SET  sku='"+sku+"', marca='"+marca+ "', tipoProducto='" +tipoProducto+ "',categoria='" +categoria+ "',color='" +color+ "',envase='" +envase+ "', tamano='" +tamano+ "',precio='" +precio+ "', cantidad='" +cantidad+ "' WHERE idproductos'"+id+"'";
+            string sql = "UPDATE productos SET sku='"+sku+"', marca='"+marca+ "',tipoProducto='"+tipoProducto+"',categoria='"+categoria+"',color='"+color+"',envase='"+envase+"',tamano='"+tamano+"',precio='"+precio+"',cantidad='"+cantidad+"' WHERE idproductos='"+id+"'";
             MySqlConnection conexionBD = Conexion.conexion();
 
             conexionBD.Open();
@@ -124,6 +125,7 @@ namespace tienda
                 MySqlCommand comando = new MySqlCommand(sql, conexionBD);
                 comando.ExecuteNonQuery();
                 MessageBox.Show("Registro Modificado");
+                limpiar();
             }
             catch (MySqlException ex)
             {
@@ -137,12 +139,47 @@ namespace tienda
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
+            string id = txtCodigo.Text;
+            
 
+            string sql = "DELETE FROM productos WHERE idproductos='"+ id +"'";
+            MySqlConnection conexionBD = Conexion.conexion();
+
+            conexionBD.Open();
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Registro Eliminado");
+                limpiar();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error al Eliminar: " + ex.Message);
+            }
+            finally
+            {
+                conexionBD.Close();
+            }
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
+            limpiar();
+        }
 
+        private void limpiar()
+        {
+            txtCodigo.Text = "";
+            txtSku.Text = "";
+            txtMarca.Text = "";
+            txtTipoProducto.Text = "";
+            txtCategoria.Text = "";
+            txtColor.Text = "";
+            txtEnvase.Text = "";
+            txtTamaño.Text = "";
+            txtPrecio.Text = "";
+            txtCantidad.Text = "";
         }
     }
 }
